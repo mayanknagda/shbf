db1fb2944b72c892f652cea039f3fee398edb3e4
mayanknagda <you@example.com>
2018-03-18 04:12:35 +0530

    finished
    finished

--- php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$currency = 'Rs: ';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'shbf';
-$db_host = 'localhost';
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
-?>
